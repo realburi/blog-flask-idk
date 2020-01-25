f6b45b72703666d9eb579aa0ba4fc2d7f3cf860a
realburi <batjargalburi@gmail.com>
2020-01-26 03:28:45 +0800

    french word dataset prep

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-import csv
-with open('names.csv','r',encoding='utf-8') as file:
-	reader = csv.reader(file)
-	interestingrows=[row for idx, row in enumerate(reader) if idx == 0]
-	print(interestingrows)
+from googletrans import Translator
+translator = Translator()
+# a = translator.translate('veritas lux mea', src='la',dest='mn')
+# print(dir(Translator))
+
+class Data:
+	def __init__(self,data):
+		self.data = data
+
+	def to_sentences(self):
+		return self.data.split(".")
+
+	def unique_words(self):
+		words = set(self.data.split())
+		return list(words)
+
+	def count(self):
+		data = self.data.split()
+		unique = len(set(data))
+		non_unique = len(data)
+		return unique, non_unique
+
+
+with open('data.txt', 'r', encoding='utf-8') as data:
+	data = Data(data.read())
+
+
+print(data.count())
